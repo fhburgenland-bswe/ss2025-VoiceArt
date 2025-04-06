c039f7b5ebf2aeaedb56d8bd6a0914b126a0e0ac
DarioDzakic <2310859034@fh-burgenland.at>
2025-04-06 22:30:46 +0200

    feat(ci): add conform and checkstyle with spotless

--- src/main/java/at/fh/burgenland/HelloController.java
+++ src/main/java/at/fh/burgenland/HelloController.java
@@ -3,12 +3,12 @@ package at.fh.burgenland;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/** JavaFX example using for pipeline test. */
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+  @FXML private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+  @FXML
+  protected void onHelloButtonClick() {
+    welcomeText.setText("Welcome to JavaFX Application!");
+  }
 }
