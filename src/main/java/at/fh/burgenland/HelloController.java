bca87684c3cb243fdfd6f01309ed1d19a8a0c10e
HeckMich <2310859029@fh-burgenland.at>
2025-04-17 14:54:20 +0200

    feat: functioning input choice and start v2

--- src/main/java/at/fh/burgenland/HelloController.java
+++ src/main/java/at/fh/burgenland/HelloController.java
@@ -19,6 +19,8 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.TargetDataLine;
 
+
+
 /** JavaFX example using for pipeline test. */
 public class HelloController {
 
@@ -45,22 +47,7 @@ public class HelloController {
   @FXML
   public void initialize() {
     loadValidAudioInputs();
-/*
-    audioInputComboBox.setOnAction(
-        event -> {
-          int selectIndex = audioInputComboBox.getSelectionModel().getSelectedIndex();
-          if (selectIndex >= 0) {
-            try {
-              TargetDataLine selectedLine =
-                  AudioLineSelector.selectLineByIndex(selectIndex, audioFormat);
-              System.out.println("Selected Input: " + audioInputComboBox.getValue());
 
-            } catch (LineUnavailableException | IllegalArgumentException e) {
-              e.printStackTrace();
-            }
-          }
-        });
-    */
 
     audioInputComboBox.setOnAction(event -> {
       int selectedIndex = audioInputComboBox.getSelectionModel().getSelectedIndex();
@@ -96,35 +83,7 @@ public class HelloController {
     });
   }
 
-  /*
-  private void loadValidAudioInputs() {
-    Mixer.Info[] mixerInfos = AudioSystem.getMixerInfo();
-    List<String> validMixerNames = new ArrayList<>();
-    List<Mixer.Info> validMixerInfos = new ArrayList<>();
-    for (Mixer.Info mixerInfo : mixerInfos) {
-      Mixer mixer = AudioSystem.getMixer(mixerInfo);
-      Info[] targetLineInfos = mixer.getTargetLineInfo();
-      for (Info lineInfo : targetLineInfos) {
-        if (lineInfo.getLineClass().equals(TargetDataLine.class)) {
-          try {
-            TargetDataLine line = (TargetDataLine) mixer.getLine(lineInfo);
-            line.open(audioFormat);
-            line.close(); // schliessen, weil wir sie ja nur kurz testen moechten
-            validMixerNames.add(mixerInfo.getName());
-            validMixerInfos.add(mixerInfo);
-            break; // fals ungueltiger Mixer, wird zu naechsten gegangen
 
-          } catch (LineUnavailableException e) {
-            // Line wird uebersprungen
-          }
-        }
-      }
-    }
-    ObservableList<String> options = FXCollections.observableArrayList(validMixerNames);
-    audioInputComboBox.setItems(options);
-  }
-
-   */
 
   private void loadValidAudioInputs() {
     Mixer.Info[] allMixerInfos = AudioSystem.getMixerInfo();
@@ -155,10 +114,14 @@ public class HelloController {
   private void startRecording(){
     if(targetDataLine != null){
       try{
+        targetDataLine.open(audioFormat);
         targetDataLine.start();
         isRecording = true;
         recordButton.setText("Stop");
         System.out.println("Recording started");
+
+
+
         //TODO: hier AudioaufnahmeLogik
       } catch (Exception e) {
         e.printStackTrace();
