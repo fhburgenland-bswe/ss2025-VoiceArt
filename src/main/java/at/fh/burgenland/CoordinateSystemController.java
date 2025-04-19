c1efc55636cae813e132aafc12b5ecff9f92e419
912Gloria912 <2310859022@fh-burgenland.at>
2025-04-19 09:48:50 +0200

    feat: integrate title and text to coordinate system scene

--- src/main/java/at/fh/burgenland/CoordinateSystemController.java
+++ src/main/java/at/fh/burgenland/CoordinateSystemController.java
@@ -2,11 +2,16 @@ package at.fh.burgenland;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /** Controller class for the coordinate system view. */
 public class CoordinateSystemController {
 
-  @FXML private Canvas canvas;
+  @FXML private Canvas coordinateSystemCanvas;
+  @FXML private Label logoLabel;
+  @FXML private Label textLabel;
+  @FXML private Button startButton;
 
   // Frequency and Loudness ranges
   private final int minFreq = 50;
@@ -14,9 +19,12 @@ public class CoordinateSystemController {
   private final int minDb = -60;
   private final int maxDb = 0;
 
+  /** Initializes the UI and draws the coordinate system. */
   @FXML
   public void initialize() {
-
-    CoordinateSystemDrawer.drawAxes(canvas, minFreq, maxFreq, minDb, maxDb);
+    logoLabel.getText();
+    textLabel.getText();
+    CoordinateSystemDrawer.drawAxes(coordinateSystemCanvas, minFreq, maxFreq, minDb, maxDb);
+    startButton.getText();
   }
 }
