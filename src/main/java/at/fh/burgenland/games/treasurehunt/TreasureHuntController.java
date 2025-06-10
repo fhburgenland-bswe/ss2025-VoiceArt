6292324758d209d999ae8a8e492aba26d482870b
Michael H. <160320818+HeckMich@users.noreply.github.com>
2025-06-10 22:37:45 +0200

    fix: fixes minus 200 db log (#49)

--- src/main/java/at/fh/burgenland/games/treasurehunt/TreasureHuntController.java
+++ src/main/java/at/fh/burgenland/games/treasurehunt/TreasureHuntController.java
@@ -304,10 +304,10 @@ public class TreasureHuntController {
             if (pitch > 0 && !Double.isInfinite(db)) {
 
               // updates session statistics
-              if (db > sessionMaxDb) {
+              if (db > sessionMaxDb && db > -100) {
                 sessionMaxDb = db;
               }
-              if (db < sessionMinDb) {
+              if (db < sessionMinDb && db > -100) {
                 sessionMinDb = db;
               }
               if (pitch > sessionMaxHz) {
